162bb9dbc6ea6bea3c8ce061e2a592323dc817f6
Bogdan <kitz_99@yahoo.com>
2014-10-05 11:16:10 +0300

    5. Controllers, views, and dynamic content

--- simple_cms/app/controllers/demo_controller.rb
+++ simple_cms/app/controllers/demo_controller.rb
@@ -3,6 +3,24 @@ class DemoController < ApplicationController
   layout false
   
   def index
-
+  	# render(:template => 'demo/hello')
   end
+
+  def hello
+  	# render(:template => 'demo/index')
+  	@array = [1,2,3,4,5]
+  	@id = params[:id];
+  	@page = params[:page]
+  	
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def google
+  	redirect_to("http://google.com")
+  end
+
+
 end
